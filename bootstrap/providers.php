a305934160c4ebce4591be1e9a3a43aed783329b
Luis Felipe Fidencio Chavez <lfelipe_ch30@hotmail.com>
2025-06-05 12:03:42 -0600

    delete old builder

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,8 +2,6 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\BlockBuilderServiceProvider::class,
     App\Providers\MoonShineServiceProvider::class,
-    App\Providers\BlockBuilderServiceProvider::class,
 
 ];
