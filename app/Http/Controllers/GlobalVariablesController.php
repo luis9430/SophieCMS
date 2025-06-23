9c7fb69fdf6eff65bdeeef4ce58d6705770e81f6
Luis Felipe Fidencio Chavez <lfelipe_ch30@hotmail.com>
2025-06-23 00:28:16 -0600

    Update GlobalVariablesController.php

--- app/Http/Controllers/GlobalVariablesController.php
+++ app/Http/Controllers/GlobalVariablesController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
 use App\Services\DesignSystemService;
+use App\Services\TailwindColorService;
 
 class GlobalVariablesController extends Controller
 {
